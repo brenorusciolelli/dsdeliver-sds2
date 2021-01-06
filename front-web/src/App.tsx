38ffc28e55333b9e00ac870f3604bd7dd53938d4
brenorusciolelli <brenorusciolelli@gmail.com>
2021-01-06 18:44:44 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
 
       <div>
-      <h1>Hellow</h1>
+      <Navbar />
        </div>
     );
     
