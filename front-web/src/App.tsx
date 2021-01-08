7639cac65cd4bda4335deb28446a8a04b0305aa7
brenorusciolelli <brenorusciolelli@gmail.com>
2021-01-08 14:22:44 -0300

    Att Bug Deploy Netlify

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,7 +2,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Routes from "./Routes"
-import Navbar from './Navbar';
+
 
 function App() {
       return (
